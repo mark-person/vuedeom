c76c6958d38e02ead6af4bd958fa997225458ee0
deng <6882855@qq.com>
2020-05-24 00:25:08 +0800

    test

--- vuedemo/src/App.vue
+++ vuedemo/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div id="app" style="border:1px solid red">
-    <el-row class="tac">
-      <el-col :span="12">
+  <div style="border:1px solid red;display: flex">
+  <div id="app" style="border:1px solid red;display: block">
+    <el-row class="tac" style="display: block;width: 205px">
+      <el-col :span="24">
         <el-menu
-          default-active="2"
+          default-active="1-2"
           class="el-menu-vertical-demo"
           @open="handleOpen"
           @close="handleClose"
@@ -12,38 +13,24 @@
           active-text-color="#ffd04b">
           <el-submenu index="1">
             <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <i class="el-icon-menu"></i>
+              <span>菜单扫描</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
+            <el-menu-item index="1-2">扫描1</el-menu-item>
+            <el-menu-item index="1-3">扫描2</el-menu-item>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>环境管理</span>
+            </template>
+            <el-menu-item index="2-2">环境1</el-menu-item>
+            <el-menu-item index="2-3">环境2</el-menu-item>
+          </el-submenu>
         </el-menu>
       </el-col>
     </el-row>
-
+  </div>
 
 
     <router-view></router-view>
--- vuedemo/src/components/menu.vue
+++ vuedemo/src/components/menu.vue
@@ -1,12 +1,103 @@
 <template>
-  <div>
-    <img src="../assets/logo.png">
-    <el-button onclick="alert(1)">默认按钮</el-button>
+  <div class="query-list" style="width:100%">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+
+
+      <el-table :data="tableData" border style="">
+        <el-table-column fixed prop="date" label="日期" width="200"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="100"></el-table-column>
+        <el-table-column prop="province" label="省份" width="100"></el-table-column>
+        <el-table-column prop="city" label="市区" width="100"></el-table-column>
+        <el-table-column prop="address" label="地址" width="250"></el-table-column>
+        <el-table-column prop="zip" label="邮编" width="100"></el-table-column>
+        <el-table-column  label="操作" width="200">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+    <el-pagination @current-change="currentChange"
+      background
+      layout="prev, pager, next"
+      :total="1000" style="padding-top:15px" >
+    </el-pagination>
+
   </div>
 </template>
 
+<style scoped>
+  .query-list {
+    padding:14px
+  }
 
+</style>
 
+<script>
+  export default {
+    data() {
+      return {
+        formInline: {
+          user: '',
+          region: ''
+        },
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1517 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1519 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1516 弄',
+          zip: 200333
+        }]
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      },
+      currentChange(val) {
+        alert(val)
+        //if (this.currentPage * val > this.total) return;
+
+      }
+    }
+  }
+</script>
 
 
 
